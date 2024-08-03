26732b5118d4b557d0d8a3f2649adc2653dd374a
Gabriel Fernandes <gfsouza@poli.ufrj.br>
2024-08-03 17:19:21 -0300

    build: swagger config

--- api/index.js
+++ api/index.js
@@ -1,10 +1,23 @@
-const { createServer } = require('http');
-const { NestFactory } = require('@nestjs/core');
-const { AppModule } = require('../dist/app.module');
+import { createServer } from 'http';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../dist/app.module';
 
-module.exports = async (req, res) => {
+export default async (req, res) => {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Eligibility API')
+    .setDescription('API to validate customer eligibilities')
+    .setVersion('1.0')
+    .addTag('Eligibility')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.enableCors();
+
   await app.init();
+
   const server = createServer(app.getHttpAdapter().getInstance());
   server.emit('request', req, res);
 };
