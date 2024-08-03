d411967890c0973d818f633a5e2ed8ee65b68f10
Gabriel Fernandes <gfsouza@poli.ufrj.br>
2024-08-03 19:00:17 -0300

    build: add nodejs runtime

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 import { createServer } from 'http';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../dist/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export default async (req, res) => {
+  // Cria a aplicação NestJS
   const app = await NestFactory.create(AppModule);
 
+  // Configuração do Swagger
   const config = new DocumentBuilder()
     .setTitle('Eligibility API')
     .setDescription('API to validate customer eligibilities')
@@ -14,10 +17,13 @@ export default async (req, res) => {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
+  // Habilita CORS
   app.enableCors();
 
+  // Inicializa a aplicação
   await app.init();
 
+  // Cria um servidor HTTP e processa as requisições
   const server = createServer(app.getHttpAdapter().getInstance());
   server.emit('request', req, res);
 };
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EligibilityModule } from './application/eligibilities/eligibility.module';
 import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), EligibilityModule],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
